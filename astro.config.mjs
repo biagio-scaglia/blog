08395b6ec1b1477c29496902e80c7b7501499b68
Biagio Scaglia <biagio.scaglia@PRISMA.local>
2026-03-04 11:29:16 +0100

    chore: switch to static sitemap manually

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,11 @@
 // @ts-check
 
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://blog.scagliabiagio99.workers.dev', // Cloudflare Workers Domain
-	integrations: [mdx(), sitemap({
-		filter: (page) => page !== 'https://blog.scagliabiagio99.workers.dev/500' && page !== 'https://blog.scagliabiagio99.workers.dev/404',
-		changefreq: 'weekly',
-		priority: 0.7,
-		lastmod: new Date(),
-	})],
+	integrations: [mdx()],
 });
