fcac4c5b12317371af41ec178517bbb389ee3d7c
Biagio Scaglia <biagio.scaglia@PRISMA.local>
2026-07-06 22:56:34 +0200

    Upgrade RSS XML generation with cover media enclosures, site fallbacks and correct location geo/aeo meta

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,13 +4,41 @@ import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
 
 export async function GET(context) {
 	const posts = await getCollection('blog');
+	const sortedPosts = posts.sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf());
+	const siteUrl = context.site ? context.site.toString().replace(/\/$/, '') : 'https://biagio-scaglia.github.io/blog';
+
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/blog/${post.id}/`,
-		})),
+		xmlns: {
+			media: 'http://search.yahoo.com/mrss/'
+		},
+		customData: `<image>
+			<url>${siteUrl}/social_share_preview.png</url>
+			<title>${SITE_TITLE}</title>
+			<link>${siteUrl}/</link>
+		</image>`,
+		items: sortedPosts.map((post) => {
+			const coverImage = post.data.cover || post.data.heroImage;
+			const imageUrl = coverImage ? (typeof coverImage === 'string' ? coverImage : coverImage.src) : null;
+			let absoluteImageUrl = `${siteUrl}/social_share_preview.png`;
+			if (imageUrl) {
+				if (imageUrl.startsWith('http')) {
+					absoluteImageUrl = imageUrl;
+				} else {
+					absoluteImageUrl = siteUrl + (imageUrl.startsWith('/') ? '' : '/') + imageUrl;
+				}
+			}
+
+			return {
+				title: post.data.title,
+				description: post.data.description,
+				pubDate: post.data.pubDate,
+				link: `/blog/blog/${post.id}/`,
+				customData: `<enclosure url="${absoluteImageUrl}" length="0" type="image/png" />
+					<media:content url="${absoluteImageUrl}" medium="image" type="image/png" />`
+			};
+		}),
 	});
 }
