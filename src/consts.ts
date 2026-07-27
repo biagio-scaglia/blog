7c12b29a0424c72c10afec3822867da79d7bbb7c
Biagio Scaglia <biagio.scaglia@PRISMA.local>
2026-07-27 09:59:18 +0200

    config: set default fallback OneSignal App ID

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,3 @@
 export const SITE_TITLE = 'Biagio Cyberspace';
 export const SITE_DESCRIPTION = "Biagio Scaglia's personal blog: full-stack developer, UX/UI designer, and lover of the retro web.";
-export const ONESIGNAL_APP_ID = import.meta.env.PUBLIC_ONESIGNAL_APP_ID || '';
+export const ONESIGNAL_APP_ID = import.meta.env.PUBLIC_ONESIGNAL_APP_ID || 'fa82b34a-977f-4d9d-ad85-93de7cf901c4';
